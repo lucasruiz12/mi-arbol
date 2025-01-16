d7870f1cee8adb3adddb493ab220e9b3ac86867f
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-01-15 21:50:12 -0300

    add payment method, new views in subscriptions list and my subscriptions, new flow and mobile view

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -3,5 +3,5 @@ export const USERS = "users";
 export const SESSION_ID_PAYMENT = "sessionIdPayment";
 export const PAYMENT_DATA = "paymentData";
 
-// export const URL_BASE = "https://backend-mi-arbol.vercel.app";
-export const URL_BASE = "http://localhost:3001";
+export const URL_BASE = "https://backend-mi-arbol.vercel.app";
+// export const URL_BASE = "http://localhost:3001";
