71012a8b7b3c28b7a03087942ce18b0d2d62d161
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-02-27 17:02:49 -0300

    resizing styles to avoid scroll

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-12700680'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fdi9b42ou0g"
+    "revision": "0.9167ha8h7cg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
