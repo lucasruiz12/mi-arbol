25ceee4db2cda167374b2b2633468cac58cda8b2
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-06-04 22:33:18 -0300

    add preregister and new changes in recharts graphics

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-12700680'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hajg7tk4qk"
+    "revision": "0.u6t0068agp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
