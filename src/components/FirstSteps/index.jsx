57e5bf1cfa06fffecc1e5ef88fc6cb9ee69a82bf
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-06-12 23:39:23 -0300

    responsive in first view 2

--- src/components/FirstSteps/index.jsx
+++ src/components/FirstSteps/index.jsx
@@ -1,76 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import logoArbol from '../../assets/logos/logo-TAO-white.svg';
 import logoISO from '../../assets/logos/logo-ISO.svg';
 import logoGreenHouse from '../../assets/logos/logo-green-house.svg';
 import CarouselLogo from '../CarouselLogos';
 import { tipsAndRecommendations } from '../../helpers/messagesAndTips';
 import BackArrow from '../BackArrow';
-// import { showPushNotification } from '../../helpers/pushNotification';
 import './style.css';
 
 const FirstSteps = ({ setView }) => {
-
     const [showMessage, setShowMessage] = useState(false);
-    const [currentMessage, setCurrentMessage] = useState("");
-
+    const [currentMessage, setCurrentMessage] = useState('');
 
     useEffect(() => {
         if (showMessage) {
             setTimeout(() => {
                 setShowMessage(false);
             }, 8000);
-            // }, 1000000000);
         } else {
-            if (currentMessage === "" || currentMessage === 3) {
-                setCurrentMessage(0)
+            if (currentMessage === '' || currentMessage === 3) {
+                setCurrentMessage(0);
             } else {
-                setCurrentMessage(currentMessage + 1)
-            };
+                setCurrentMessage(currentMessage + 1);
+            }
             setTimeout(() => {
                 setShowMessage(true);
             }, 1000);
-        };
+        }
     }, [showMessage]);
 
     return (
-        <div className="container-steps-view">
+        <div className="container-steps-view container-fluid h-100">
             <BackArrow handleAction={() => setView(1)} />
-            <div className="steps-content">
-                <div className="steps-img-container">
-                    <img className="steps-logo" src={logoArbol} alt="LOG" />
-                    <div className="steps-logos">
-                        <img src={logoGreenHouse} alt="NOIMG" className="logo-iso" />
-                        <img src={logoISO} alt="NOIMG" className="logo-iso" />
-                    </div>
+            {currentMessage !== '' && showMessage && (
+                <p className="steps-tips">{tipsAndRecommendations[currentMessage].message}</p>
+            )}
+            <div className="steps-content row h-100 ms-md-4 align-content-around">
+                <div className="steps-img-container col-12 d-flex justify-content-md-between justify-content-center align-items-center">
+                    <img
+                        className="steps-logo"
+                        src={logoArbol}
+                        alt="Logo de Más Raíces, Menos Huella"
+                    />
                 </div>
-                {
-                    currentMessage !== "" && showMessage &&
-                    <p className="steps-tips">{tipsAndRecommendations[currentMessage].message}</p>
-                }
-                <div className="steps-info-container">
-                    <div className="steps-text">
-                        <p className="steps-line-text">El primer paso que debes dar es medir los gases de efecto invernadero que generas en tu vida diaria.</p>
-                        <p className="steps-line-text">Contestando unas preguntas muy sencillas y apegándonos a los factores de emisión del <i>Protocolo Internacional GHG</i>, podemos determinar un cálculo preciso de cuanto debemos mitigar para ser carbon neutro.</p>
-                    </div>
-                </div>
-                <div style={{ display: "flex", flexDirection: "column", width: "30%", alignItems: "center" }}>
-                    <Link className="link-btn" to="/initQuestions">
-                        <button className="btn-green">
-                            Medir mi huella
-                        </button>
-                    </Link>
-                    <div>
-                        <p style={{ color: "white", marginTop: "0.5rem" }}>
-                            ¿Tienes una cuenta?
-                            <Link style={{ paddingTop: "1rem", fontSize: "1rem", marginLeft: "0.25rem" }} to="/loginForm">
-                                Inicia sesión
-                            </Link>
+                <div className="steps-info-container d-flex justify-content-between col-12">
+                    <div className="steps-text col-12 col-md-8 col-lg-6">
+                        <p className="steps-line-text">
+                            El primer paso que debes dar es medir los gases de efecto invernadero que generas en tu vida diaria.
+                        </p>
+                        <p className="steps-line-text">
+                            Contestando unas preguntas muy sencillas y apegándonos a los factores de emisión del <i>Protocolo Internacional GHG</i>, podemos determinar un cálculo preciso de cuanto debemos mitigar para ser carbon neutro.
                         </p>
                     </div>
+                    <div className="steps-logos gap-4">
+                        <img
+                            src={logoGreenHouse}
+                            alt="Logo de Green House Protocol"
+                            className="logo-green-house"
+                        />
+                        <img src={logoISO} alt="Logo de ISO" className="logo-iso" />
+                    </div>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <div className="col-12 col-md-5 col-lg-4 d-flex flex-column align-items-center">
+                    <Link className="link-btn btn btn-green w-100 mb-2" to="/initQuestions">
+                        Medir mi huella
+                    </Link>
+                    <p className="text-white mb-0">
+                        ¿Tienes una cuenta?{' '}
+                        <Link className="text-blue" to="/loginForm">
+                            Inicia sesión
+                        </Link>
+                    </p>
+                </div>
+                <div className="col-12 d-flex flex-column align-items-center">
                     <p className="steps-clients-title">Nuestros clientes</p>
                     <CarouselLogo />
                 </div>
--- src/components/OurMission/index.jsx
+++ src/components/OurMission/index.jsx
@@ -4,72 +4,78 @@ import { tipsAndRecommendations } from '../../helpers/messagesAndTips';
 import './style.css';
 
 const OurMission = ({ setView }) => {
+  const [showMessage, setShowMessage] = useState(false);
+  const [hideMessage, setHideMessage] = useState(false);
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [notTimer, setNotTimer] = useState(false);
 
-    const [showMessage, setShowMessage] = useState(false);
-    const [hideMessage, setHideMessage] = useState(false);
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [notTimer, setNotTimer] = useState(false);
+  const handleClick = () => {
+    setView(2);
+    setNotTimer(true);
+    removeClick();
+  };
 
-    const handleClick = () => {
-        setView(2);
-        setNotTimer(true);
-        removeClick();
-    };
+  const removeClick = () => {
+    document.removeEventListener('click', handleClick);
+  };
 
-    const removeClick = () => {
-        document.removeEventListener("click", handleClick);
-    };
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => removeClick();
+  }, []);
 
-    useEffect(() => {
-        document.addEventListener("click", handleClick);
-    }, []);
+  useEffect(() => {
+    if (!notTimer) {
+      if (showMessage) {
+        setTimeout(() => {
+          setShowMessage(false);
+          if (currentMessage === 3) {
+            setView(2);
+            removeClick();
+          }
+        }, 8000);
+      } else {
+        if (currentMessage === '') {
+          setCurrentMessage(0);
+        } else {
+          if (currentMessage < 3) {
+            setCurrentMessage(currentMessage + 1);
+          }
+        }
+        setTimeout(() => {
+          setShowMessage(true);
+          setHideMessage(false);
+        }, 1000);
+      }
+    }
+  }, [showMessage]);
 
-    useEffect(() => {
-        if (!notTimer) {
-            if (showMessage) {
-                setTimeout(() => {
-                    setShowMessage(false);
-                    if (currentMessage === 3) {
-                        setView(2);
-                        removeClick();
-                    };
-                }, 8000);
-                // }, 1000000000);
-            } else {
-                if (currentMessage === "") {
-                    setCurrentMessage(0)
-                } else {
-                    if (currentMessage < 3) {
-                        setCurrentMessage(currentMessage + 1)
-                    };
-                };
-                setTimeout(() => {
-                    setShowMessage(true);
-                    setHideMessage(false);
-                }, 1000);
-            };
-        };
-    }, [showMessage]);
-
-    return (
-        <div className="container-mission-view">
-            <div className="mission-content">
-                <div className="mission-header">
-                    <img /*onClick={() => setView(2)}*/ className="mission-logo" src={logoArbol} alt="LOG" />
-                    {
-                        currentMessage !== "" && showMessage && !hideMessage &&
-                        <p className="mission-tips">{tipsAndRecommendations[currentMessage].message}</p>
-                    }
-                </div>
-                <div className="mission-info-container">
-                    <div className="mission-text">
-                        <p className="mission-line-text">En “Más Raíces, Menos Huella” te ayudamos a calcular tus emisiones diarias para entender el impacto que tienes como persona.</p>
-                        <p className="mission-line-text">Al entender cuanto CO2 generas, te ayudaremos a reforestar los suficientes árboles para mitigar tu huella de carbono.</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container-mission-view container-fluid h-100">
+      <div className="mission-content row h-100 ms-md-4 align-items-md-around">
+        <div className="mission-header col-12 d-flex justify-content-md-between justify-content-center align-items-center">
+          <img
+            className="mission-logo"
+            src={logoArbol}
+            alt="Logo de Más Raíces, Menos Huella"
+          />
+          {currentMessage !== '' && showMessage && !hideMessage && (
+            <p className="mission-tips">{tipsAndRecommendations[currentMessage].message}</p>
+          )}
         </div>
-    );
+        <div className="mission-info-container col-12">
+          <div className="mission-text col-12 col-md-8 col-lg-6">
+            <p className="mission-line-text">
+              En “Más Raíces, Menos Huella” te ayudamos a calcular tus emisiones diarias para entender el impacto que tienes como persona.
+            </p>
+            <p className="mission-line-text">
+              Al entender cuanto CO2 generas, te ayudaremos a reforestar los suficientes árboles para mitigar tu huella de carbono.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurMission;
