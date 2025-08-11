6284f7057eee0a5ab40c095c77abbec1b9c49c2c
@leandro.rchosco <leandro.rchosco@gmail.com>
2025-08-10 22:23:54 -0600

    ajuste letra en our Mission

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-12700680'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dum0q9td0d"
+    "revision": "0.8q35i16fmf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
